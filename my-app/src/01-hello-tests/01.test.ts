98d797c9508fb4b5c27127143be26dc6475e36a6
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-08-21 19:45:29 +0200

    tests

---
+++ my-app/src/01-hello-tests/01.test.ts
@@ -0,0 +1,13 @@
+import {sum} from "./01";
+
+test('test should be correct', ()=>{
+    //data
+    const a = 1
+    const b = 2
+    const c = 3
+
+    //action
+    const result = sum(a, b)
+
+    //expect result
+})
