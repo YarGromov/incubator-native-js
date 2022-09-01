83d14b3c12754b7a30644cbb2d8bc60382cd6646
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-09-01 21:23:35 +0200

    completedTasks

--- my-app/src/04-filter/04.test.ts
+++ my-app/src/04-filter/04.test.ts
@@ -19,13 +19,28 @@ test('should take courses cheaper $160', ()=>  {
         {title: 'REACT', price: 150},
     ]
 
-    const cheapPredicate = (course: CoursePropsType) => {
-        return course.price < 160
-    }
 
-    const cheapCourse = courses.filter(cheapPredicate)
+
+    const cheapCourse = courses.filter(course => course.price < 160)
 
     expect(cheapCourse.length).toBe(2)
     expect(cheapCourse[0].price).toBe(110)
+    expect(cheapCourse[1].title).toBe('REACT')
+
+})
+
+test('get only completed tasks', ()=> {
+    let tasks = [
+        {id: 1, title: 'Bread', isDone: false},
+        {id: 2, title: 'Milk', isDone: true},
+        {id: 3, title: 'Solt', isDone: false},
+        {id: 4, title: 'Sugar', isDone: true}
+    ]
+
+    const completedTasks = tasks.filter(task => task.isDone)
+
+    expect(completedTasks.length).toBe(2)
+    expect(completedTasks[0].id).toBe(2)
+    expect(completedTasks[1].id).toBe(4)
 
 })
