9ba4c018d85adc3b629047abc9c4783e50db8f32
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-09-08 20:11:36 +0200

    users.map

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+    const names = ['Oleg', 'Katya', 'Pasha', 'Tanya']
+    const users = [
+        {name: 'Oleg Oleg'},
+        {name: 'Katya Katya'},
+        {name: 'Pasha Pasha'},
+        {name: 'Tanya Tanya'},
+    ]
+    const liElements = users.map((u, index)=> <li key={index}>{index} - {u.name}</li>)
   return (
     <div className="App">
-      <h1>Hello Yopt</h1>
+      <ul>
+          {liElements}
+      </ul>
     </div>
   );
 }
