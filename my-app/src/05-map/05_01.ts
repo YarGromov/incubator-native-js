e10a9b5a9882747ea475a9acb753284767be5ece
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-09-08 19:29:56 +0200

    map

---
+++ my-app/src/05-map/05_01.ts
@@ -0,0 +1,52 @@
+export type ManType = {
+    name: string
+    age: number
+}
+
+const people: Array<ManType> = [
+    {name: 'Andrew Ivanov', age: 33},
+    {name: 'Alex Petrov', age: 24},
+    {name: 'Dmitry Sidorov', age: 18},
+]
+
+const dimychTransformator = (man: ManType) => {
+    return {
+        stack: ['css', 'html', 'js', 'tdd', 'react'],
+        firstName: man.name.split(' ')[0],
+        lastName: man.name.split(' ')[1]
+    }
+}
+
+const devs1 = [
+    {
+        stack: ['css', 'html', 'js', 'tdd', 'react'],
+        firstName: 'Andrew', lastName: 'Ivanov'
+    }, {
+        stack: ['css', 'html', 'js', 'tdd', 'react'],
+        firstName: 'Alex', lastName: 'Petrov'
+    }, {
+        stack: ['css', 'html', 'js', 'tdd', 'react'],
+        firstName: 'Dmitry', lastName: 'Sidorov'
+    },
+]
+
+const devs2 = [
+    dimychTransformator( people[0] ),
+    dimychTransformator( people[1] ),
+    dimychTransformator( people[2] )
+]
+
+
+const devs3 = people.map(dimychTransformator)
+
+const devs4 = people.map(man => ({
+    stack: ['css', 'html', 'js', 'tdd', 'react'],
+    firstName: man.name.split(' ')[0],
+    lastName: man.name.split(' ')[1]
+}))
+
+const message = people.map( man => {
+    return (
+        `Hello ${man.name.split(' ')[0]}. Welcome to IT-Incubator`
+    )
+})
