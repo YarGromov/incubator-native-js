9aae54d29e8e6c4f7a09a45fd361fe7ef21be51e
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-09-11 15:25:48 +0200

    Object.keys/values

--- my-app/src/08-associative-array/associative.tsx
+++ my-app/src/08-associative-array/associative.tsx
@@ -8,6 +8,24 @@ let user = {
     }
 }
 
-let userTitle1=  user.address.city.title
-let userTitle2 = user['address']['city']['title']
+let userTitle1 = user.address.city.title //Minsk
+let userTitle2 = user['address']['city']['title'] //Minsk
 
+
+let users = ["Natasha", "Natasha1", "Natasha2", "Natasha3", "Natasha4"];
+users['map']((el) => el.toUpperCase())
+users.map((el) => el.toLowerCase())
+
+let usersObj: any = {
+    '0': 'Natasha',
+    '1': 'Vadim',
+    '2': 'Kolya',
+    '3': 'Sveta'
+}
+usersObj['0'] ;// Natasha
+
+usersObj["привет как дела"] = 'хорошо';
+
+Object.keys(usersObj) // ['0', '1', '2', '3', 'привет как дела']
+
+Object.values(usersObj) // ['Natasha', 'Vadim', 'Kolya', 'Sveta', 'хорошо']
