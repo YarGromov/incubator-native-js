e682913501bcc691c83d9fbd9f273ded53e522e5
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-11-22 18:31:27 +0100

    immutability, makeHairStyle

--- my-app/src/10-immutability/10_01.test.ts
+++ my-app/src/10-immutability/10_01.test.ts
@@ -1,166 +1,27 @@
-import {
-    addNewBooksToUser,
-    makeHairstyle, removeBook,
-    updateBook, updateCompanyTitle, updateCompanyTitle2,
-    upgradeUserLaptop,
-    UserWithBooksType,
-    UserWithLaptopType, WithCompanyType
-} from "./10_01";
+import {UserType} from "./10_01";
 
 
+function makeHairStyle(u: UserType, power: number){
+    const copy = {
+        ...u,
+        hair: u.hair / power
+    }
+    // copy.hair = u.hair / power
+    return copy
+}
 
-
-test('reference type test', () => {
-
-    let user: UserWithLaptopType = {
+test('reference type test', ()=>{
+    let user: UserType = {
         name: 'Dimych',
         hair: 32,
         address: {
             title: 'Minsk'
-        },
-        laptop: {
-            title: 'Zenbook'
         }
-
     }
-    const awesomeUser = makeHairstyle(user, 2)
-    expect(awesomeUser.hair).toBe(16)
+  const awesomeUser = makeHairStyle(user, 2);
+
     expect(user.hair).toBe(32)
+    expect(awesomeUser.hair).toBe(16)
     expect(awesomeUser.address).toBe(user.address)
-})
-
-test('upgrade laptop to Macbook', () => {
-
-    let user: UserWithLaptopType = {
-        name: 'Dimych',
-        hair: 32,
-        address: {
-            title: 'Minsk'
-        },
-        laptop: {
-            title: 'Zenbook'
-        }
-    }
-    const userCopy  = upgradeUserLaptop(user,  "Mackbook");
-
-    expect(user).not.toBe(userCopy)
-    expect(user.address).toBe(userCopy.address)
-    expect(user.laptop).not.toBe(userCopy.laptop)
-    expect(userCopy.laptop.title).toBe('Mackbook')
-    expect(user.laptop.title).toBe('Zenbook')
 
 })
-
-test('add new books to user', () => {
-
-    let user: UserWithLaptopType & UserWithBooksType = {
-        name: 'Dimych',
-        hair: 32,
-        address: {
-            title: 'Minsk'
-        },
-        laptop: {
-            title: 'Zenbook'
-        },
-        books: ['html', 'css', 'js', 'react']
-    }
-    const userCopy  = addNewBooksToUser(user,  'ts');
-
-    expect(user).not.toBe(userCopy)
-    expect(user.laptop).toBe(userCopy.laptop)
-    expect(user.address).toBe(userCopy.address)
-    expect(user.books).not.toBe(userCopy.books)
-    expect(userCopy.books[4]).toBe('ts')
-    // expect(userCopy.books[5]).toBe('rest api')
-
-})
-
-test('update js to ts', () => {
-
-    let user: UserWithLaptopType & UserWithBooksType = {
-        name: 'Dimych',
-        hair: 32,
-        address: {
-            title: 'Minsk'
-        },
-        laptop: {
-            title: 'Zenbook'
-        },
-        books: ['html', 'css', 'js', 'react']
-    }
-    const userCopy  = updateBook(user, 'js',  'ts');
-
-    expect(user).not.toBe(userCopy)
-    expect(user.laptop).toBe(userCopy.laptop)
-    expect(user.address).toBe(userCopy.address)
-    expect(user.books).not.toBe(userCopy.books)
-    expect(userCopy.books[2]).toBe('ts')
-})
-
-test('remove js', () => {
-
-    let user: UserWithLaptopType & UserWithBooksType = {
-        name: 'Dimych',
-        hair: 32,
-        address: {
-            title: 'Minsk'
-        },
-        laptop: {
-            title: 'Zenbook'
-        },
-        books: ['html', 'css', 'js', 'react']
-    }
-    const userCopy  = removeBook(user, 'js');
-
-    expect(user).not.toBe(userCopy)
-    expect(user.laptop).toBe(userCopy.laptop)
-    expect(user.address).toBe(userCopy.address)
-    expect(user.books).not.toBe(userCopy.books)
-    expect(userCopy.books[2]).toBe('react')
-})
-
-test('with companies', () => {
-
-    let user: UserWithLaptopType & WithCompanyType = {
-        name: 'Dimych',
-        hair: 32,
-        address: {
-            title: 'Minsk'
-        },
-        laptop: {
-            title: 'Zenbook'
-        },
-        companies: [{id: 1, title: 'епам'},{id: 2, title: 'IT-INCUBATOR'}]
-    }
-    const userCopy = updateCompanyTitle(user, 1, 'EPAM') as  UserWithLaptopType & WithCompanyType;
-
-    expect(user).not.toBe(userCopy)
-    expect(user.address).toBe(userCopy.address)
-    expect(user.companies).not.toBe(userCopy.companies)
-    expect(userCopy.companies[0].title).toBe('EPAM')
-})
-
-test('update company', () => {
-
-    let user: UserWithLaptopType = {
-        name: 'Dimych',
-        hair: 32,
-        address: {
-            title: 'Minsk'
-        },
-        laptop: {
-            title: 'Zenbook'
-        }
-    }
-    let companies = {
-        'Dimych': [{id: 1, title: 'епам'},{id: 2, title: 'IT-INCUBATOR'}],
-        'Artem': [{id: 2, title: 'IT-INCUBATOR'}]
-    }
-
-   const copy = updateCompanyTitle2(companies, 'Dimych', 1, 'EPAM')
-
-    expect(copy['Dimych']).not.toBe(companies['Dimych'])
-    expect(copy['Artem']).toBe(companies['Artem'])
-    expect(copy['Dimych'][0].title).toBe('EPAM')
-})
-
